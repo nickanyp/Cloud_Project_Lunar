0a30d9fe5218397fa8f8bb6ea8d48519c9891c70
bamkornkamol <88421920+bamkornkamol@users.noreply.github.com>
2024-03-05 16:41:01 +0700

    Add renter

--- frontend/src/views/AddRenter.vue
+++ frontend/src/views/AddRenter.vue
@@ -1,35 +1,22 @@
 <template>
     <section id="app" style="font-family: 'Prompt', sans-serif;">
         <NavBar />
-        <div class="container mx-auto my-auto p-6 flex flex-col h-screen w-full items-center">
-            <div class="w-3/5 bg-slate-200 rounded-xl mt-5 p-5">
-                <h1 class="text-2xl font-bold mb-6">Room 101</h1>
-                <div class="grid grid-cols-2 gap-4 mb-4 ml-10">
-                    <div>
-                        <p class="mb-2">
-                            <span class="font-bold">เลขห้อง:</span> {roomData.num}
-                        </p>
-                        <p class="mb-2">
-                            <span class="font-bold">ชื่อผู้เช่า1:</span> {roomData.name1}
-                        </p>
-                        <p class="mb-2">
-                            <span class="font-bold">ชื่อผู้เช่า2:</span> {roomData.name2}
-                        </p>
-                        </div>
-                        <div>
-                        <p class="mb-2">
-                            <span class="font-bold">ค่าน้ำ:</span> {roomData.water} หน่วย
-                        </p>
-                        <p class="mb-2">
-                            <span class="font-bold">ค่าไฟ:</span> {roomData.fire} หน่วย
-                        </p>
-                        <p class="mb-2">
-                            <span class="font-bold">ราคาห้อง:</span> {roomData.price} บาท
-                        </p>
-                    </div>
-                </div>
-                <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600" onClick={handleSave}>Save</button>
-            </div>
+        <div class="flex flex-col items-center h-screen mt-14">
+            <h1 class="text-4xl mb-6">เพิ่มรายละเอียดผู้เช่า</h1>
+            <input type="text" placeholder="เลขห้อง" class="border border-gray-300 rounded-xl p-3 mb-3 w-5/12"/>
+            <select class="border border-gray-300 rounded-xl p-3 mb-3 w-5/12" name="" id="">
+                <option value="">ห้องเดี่ยว</option>
+                <option value="">ห้องเดี่ยวเครื่องปรับอากาศ</option>
+                <option value="">ห้องคู่</option>
+                <option value="">ห้องคู่เครื่องปรับอากาศ</option>
+            </select>
+            <input type="text" placeholder="ราคา" class="border border-gray-300 rounded-xl p-3 mb-3 w-5/12"/>
+            <input type="text" placeholder="E-mail" class="border border-gray-300 rounded-xl p-3 mb-3 w-5/12"/>
+            <input type="text" placeholder="ชื่อผู้เช่า1" class="border border-gray-300 rounded-xl p-3 mb-3 w-5/12"/>
+            <input type="text" placeholder="เบอร์โทรผู้เช่า1" class="border border-gray-300 rounded-xl p-3 mb-3 w-5/12"/>
+            <input type="text" placeholder="ชื่อผู้เช่า2" class="border border-gray-300 rounded-xl p-3 mb-3 w-5/12"/>
+            <input type="text" placeholder="เบอร์โทรผู้เช่า2" class="border border-gray-300 rounded-xl p-3 mb-3 w-5/12"/>
+            <button class="bg-[#2E4E73] hover:bg-gray-500 text-white px-4 py-2 rounded">Save</button>
         </div>
         <!-- <FooterBar/> -->
     </section>
