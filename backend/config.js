b1771280a8feeb8c2a82183df9cb1b008659a95a
nickanyp <85357272+nickanyp@users.noreply.github.com>
2024-03-11 04:32:40 +0700

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 //Database Configuration
 
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: 'db-lunar-rds.cjygw2mieoqy.ap-southeast-1.rds.amazonaws.com',
