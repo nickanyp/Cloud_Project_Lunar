765ad4dbcbb37f5d4f691484f49627a3ff2893df
nickanyp <85357272+nickanyp@users.noreply.github.com>
2024-03-11 03:39:55 +0700

    backend

--- backend/index.js
+++ backend/index.js
@@ -10,14 +10,14 @@ app.use(express.static('static'))
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-// routers    
+// routers
 const cloudRouter = require('./routes/cloud')
 
-app.use(cloudRouter.router)
+app.use(cloudRouter.router) 
 
-app.listen(3000, () => {
-  console.log(`Example app listening at http://localhost:3000`)
+app.listen(3001, () => {
+  console.log(`Example app listening at http://localhost:3001`)
 })
 
-// app.get('/', (req, res) => res.json(`API running`))
+app.get('/', (req, res) => res.json(`API running`))
 
